dbf1325476e7e2b3869cf23cb7406bfb3972317f
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-08-08 11:52:02 -0500

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 // BrowserModule includes a kit of "built in" browser-ready features.
 // It avoids the need to import specific features: *ngIf, *ngFor
--- src/app/user-dashboard/user-dashboard.module.ts
+++ src/app/user-dashboard/user-dashboard.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { UserDashboardComponent } from './user-dashboard.component';
