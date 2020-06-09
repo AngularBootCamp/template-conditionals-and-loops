8c28082ca433847f550b4a289b4cfaab91f6f884
John Baur <john.baur@oasisdigital.com>
2020-06-09 08:19:50 -0500

    curriculum update

--- src/app/employee-browser/employee-browser.module.ts
+++ src/app/employee-browser/employee-browser.module.ts
@@ -5,7 +5,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmployeeBrowserComponent } from './employee-browser.component';
 
 const routes: Routes = [
-  { path: '', component: EmployeeBrowserComponent, pathMatch: 'full' }
+  {
+    path: '',
+    component: EmployeeBrowserComponent,
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
